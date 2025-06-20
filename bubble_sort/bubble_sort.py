f6203ecd6ae4c3f9f6970867ad8d9ea61bb44330
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-06-20 12:10:31 +0300

    bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,16 @@
 def bubble_sort(unsorted_list):
-    n = len(unsorted_list)
+    arr = list(unsorted_list)
+    n = len(arr)
+
     for i in range(n - 1):
         swapped = False
+        
         for j in range(0, n - 1 - i):
-            if unsorted_list[j] > unsorted_list[j+1]:
-                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
+        
         if not swapped:
             break
-    return unsorted_list
+            
+    return arr
