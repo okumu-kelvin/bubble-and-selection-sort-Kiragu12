9f38c4963bbe5722bb8e8b1af86457731b4665db
Kiragu12 <macharia.kiragu@strathmore.edu>
2025-06-20 12:12:03 +0300

    selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -11,9 +11,7 @@ def selection_sort(arr):
             if arr[j] < arr[min_idx]:
                 min_idx = j
 
-        # Swap the found minimum element with the first element of the unsorted part
-        # (which is arr[i])
-        # Only swap if the minimum element is not already in its correct place
+        
         if min_idx != i: # This check makes it slightly more efficient if element is already in place
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
